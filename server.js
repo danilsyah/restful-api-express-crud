8295201fd49fbb79bbff67896c401778982ebcc8
danilsyah <danilsyaharihardjo@gmail.com>
2024-02-13 06:49:00 +0700

    API-009 mod feature CORS library

--- server.js
+++ server.js
@@ -1,31 +1,36 @@
-// import libraries
+// import library dot environment
 require("dotenv").config();
-const bodyParser = require('body-parser');
-const express = require('express');
-
+// import librarty body-parser
+const bodyParser = require("body-parser");
+// import library express
+const express = require("express");
+// import library CORS
+const cors = require("cors");
 // object app
 const app = express();
 // set port, listen for requests
 const PORT = process.env.NODE_DOCKER_PORT || 3000;
-
+// use cors
+app.use(cors());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Service is Running Nodejs Express Restful API');
-})
+app.get("/", (req, res) => {
+  res.send("Service is Running Nodejs Express Restful API");
+});
 
 // import route posts
-const postsRouter = require('./routes/posts');
-app.use('/api/posts', postsRouter);
+const postsRouter = require("./routes/posts");
+app.use("/api/posts", postsRouter);
 
-
+// run app
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
-
+  console.log(`Server is running on port ${PORT}`);
+});
